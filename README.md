888a2446c57ce78b33937297d9946aa85a96d509
Gruz <arygroup@gmail.com>
2016-07-03 21:58:02 +0300

    1.0.5 stable

---
+++ README.md
@@ -0,0 +1,5 @@
+# RegisterGoBack
+
+Redirects a user to the page where one clicked on the `Register` button. Usefull when you want a `Register to readmore` to be redirected to the full article when registred and logged in. Loggs in the user automatically on email confirmation link click
+
+*See for more information:* http://gruz.org.ua/extensions/registergoback.html
---
+++ registergoback.php
@@ -0,0 +1,330 @@
+<?php
+/**
+ * RegisterGoBack plugin
+ *
+ * @package		RegisterGoBack
+ * @author Gruz <arygroup@gmail.com>
+ * @copyright	Copyleft - All rights reversed
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+
+jimport('joomla.event.plugin');
+
+class plgSystemRegistergoback extends JPlugin
+{
+	var $session_namespace = 'N;.pJ8"w';// Just to be sure not to confuse with any other extension
+	var $component = 'com_users';
+
+	/**
+	 * This event is triggered after the framework has loaded and initialised and the router has route the client request.
+	 * Routing is the process of examining the request environment to determine which component should receive the request. This component optional parameters are then set in the request object to be processed when the application is being dispatched.
+	 * When this event triggers the router has parsed the route and pushed the request parameters into JRequest for retrieval by the application.
+	 *
+	 * @return none
+	 */
+	function onAfterRoute() {
+		$options_to_exclude = array('com_dump','com_jcomments');
+		if (in_array(JFactory::getApplication()->input->get('option'),$options_to_exclude)  ) { return true; }
+		if (JFactory::getApplication()->input->get('option') == 'com_aicontactsafe' && in_array(JFactory::getApplication()->input->get('task'),array('captcha','message'))  ) { return true; }
+		// Some basic variables to work
+		$app	= JFactory::getApplication();
+		// We do not need to do anything, if the user is logged in
+		if (!JFactory::getUser()->guest) {return true;}
+		if ($app->isAdmin()) {return true;}
+
+		$jinput = $app->input;
+
+		$session = JFactory::getSession();
+
+		// If the user is a guest, then we must store current link to Session each time
+		// one follows a web-site link. So at the moment, when the users clicks on "Register"
+		// OR(maybe) login, we fixate the last link as the returnLink
+
+		// Перевіряємо, чи відбувся перехід з не com_users на com_users. Якщо відбувся, то попередній лінк фіксуємо як returnLink.
+		// А для цього коже раз ще зберігаємо поточний компонент у сесію, щоби наступного разу порівняти його із вже новим поточним.
+		// Якщо не відбувся, то попередній лінк затираємо поточним.
+		$prev_component = $session->get('previous_component', null, $this->session_namespace);
+
+		$session->set('previous_component', $jinput->get('option'), $this->session_namespace);
+		//if ($jinput->get('option') == $this->component && $prev_component == $this->component ) { return true; }
+
+		$prev_link = $session->get('previous_link', '', $this->session_namespace);
+
+
+		$url = $jinput->get('return',false);
+		 if ($jinput->get('option') == $this->component) {
+			$url = $prev_link;
+		}
+		else if ($url) {
+			$url = base64_decode($url);
+		}
+		else {
+			// Here I "unparse" the Joomla SEF url to get the internal joomla URL
+			//~ JURI::current();// It's very strange, but without this line at least Joomla 3 fails to fulfill the task
+			//~ $router = JSite::getRouter();// get router
+			//~ $juri = JURI::getInstance();
+			//~ $query = $router->parse($juri); // Get the real joomla query as an array - parse current joomla link
+			//~ $url = 'index.php?'.JURI::getInstance()->buildQuery($query);
+
+			// build the JInput object
+			$jinput = JFactory::getApplication()->input;
+			// retrieve the array of values from the request (stored in the application environment) to form the query
+			$uriQuery = $jinput->getArray();
+			// build the the query as a string
+			$url = 'index.php?' . JUri::buildQuery($uriQuery);
+		}
+		// Just store current link to session to use it
+		$session->set('previous_link', $url, $this->session_namespace);
+
+		// If current link is the link to activate a user, then we run our custom functio to activate and login user
+		if ($jinput->get('option') == $this->component && $jinput->get('task') == 'registration.activate') {
+			$this->activateAndLoginUser();
+			return true;
+		}
+
+		if ($jinput->get('option') == $this->component && $prev_component != $this->component ) {
+			//get $return value as explained here http://docs.joomla.org/How_do_you_redirect_users_after_a_successful_login%3F
+			$return = $jinput->get('return',false);
+			// If there is such a return value passed, the we decode it and store to the session as returnLink.
+			// So we store the initial return link into the session and have in in the Session while "surfing" registration process.
+			// When registration process is finished, the link is stored to the newly user to be used for redirect.
+			if ($return) {
+				$return = base64_decode($jinput->get('return'));
+				$session = JFactory::getSession();
+				$session->set('returnLink', $return, $this->session_namespace);
+			}
+			else  {
+				$session->set('returnLink', $prev_link, $this->session_namespace);
+				//$session->clear('previous_link', $this->session_namespace);
+			}
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Add the redirect link to the user params to be used once
+	 *
+	 * @author Gruz <arygroup@gmail.com>
+	 */
+	function onUserAfterSave($data, $isNew, $result, $error)	{
+
+		// Some basic variables to work
+		$app	= JFactory::getApplication();
+		// We do not need to do anything, if the user is logged in
+		if (!JFactory::getUser()->guest) {return true;}
+		if ($app->isAdmin()) {return true;}
+
+
+		if (!$isNew) { return true;}
+		//static $already = false;
+		//if ($already) { return true; }
+		//$already = true;
+
+		$user = JFactory::getUser($data['id']);
+
+		$session = JFactory::getSession();
+		$redirect_url = $session->get('returnLink', null, $this->session_namespace);
+		$session->clear('returnLink', $this->session_namespace);
+
+		$user->setParam('returnLink', $redirect_url);
+		$user->save();
+
+
+		return true;
+	}
+
+
+
+	/**
+	 * Activates the users by email link or by manually entered token. Logins user.
+	 *
+	 * We are here, if we click a registration activation link or enter the activation token manually
+	 * We cannot catch the moment when a user is activated by Joomla (there is no event like onUserActivate).
+	 * So to login user on activation complete we activete the user manually and login one.
+	 * To login we must know the user password, which is not possible. So before login we store the existing
+	 * password hash to the session, set a new temorary password, use the new password to login.
+	 * Later, onUserLogin, we restore the original password hash
+	 *
+	 * @author Gruz <arygroup@gmail.com>
+	 * @return	bool or void
+	 */
+	function activateAndLoginUser () {
+		if (!$this->params->get('autologinonactivate', 1)) {return;}
+		$app	= JFactory::getApplication();
+		$jinput = $app->input;
+
+		if ($jinput->get('option') != $this->component && $jinput->get('task') != 'registration.activate' ) {
+			return false;
+		}
+
+		// If the user is logged in, return them back to the redirect URL.
+		//if ($user->get('id')) {
+		//	$this->setRedirect($redirect_url);
+		//	return true;
+		//}
+
+		//Get com_users options
+		$uParams	= JComponentHelper::getParams($this->component);
+
+		// If user registration or account activation is disabled, throw a 403.
+		if ($uParams->get('useractivation') == 0 || $uParams->get('allowUserRegistration') == 0) {
+			JError::raiseError(403, JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'));
+			return false;
+		}
+
+		//Get the activation token
+		$token = $jinput->get('token', null, 'request', 'alnum');
+
+		// Check that the token is in a valid format.
+		if ($token === null || strlen($token) !== 32) {
+			JError::raiseError(403, JText::_('JINVALID_TOKEN'));
+			return false;
+		}
+
+		// Get the user which has the current token form DB to
+		// temporary store one's password. We need this to perform
+		// autologin. We cannot decode the existing password, so we
+		// use a temporary known password, login and then after
+		// login restore the preserved password directly in the
+		// database
+		$db		= JFactory::getDBO();
+		$query	= $db->getQuery(true);
+		$query->select('id,username, email, password');
+		$query->from('#__users');
+		$query->where('activation=' . $db->Quote($token));
+		$db->setQuery($query);
+		$result = $db->loadObject();
+		if (empty($result)) {
+			return true;
+		}
+		//preserve original password
+		$session = JFactory::getSession();
+		$session->set('origPass', $result->password, $this->session_namespace);
+
+		//Set a temporary password for the user
+		$temp_pass = JApplicationHelper::getHash(JUserHelper::genRandomPassword());
+
+		$query	= $db->getQuery(true);
+		$query->update ('#__users');
+		$query->set('password = '.$db->Quote(md5($temp_pass)));
+		$query->where('id=' . $db->Quote($result->id));
+		$db->setquery($query);
+		$db->execute();
+		//~ $getaffectedrows = $db->getAffectedRows();
+//~ dump ($getaffectedrows,'$res');
+
+
+		//~ $db->query($query);
+		// Attempt to activate the user.
+		//first we import some help from the joomla framework
+		JLoader::import('joomla.application.component.model');
+		//Load and get com_users's model
+		JLoader::import( 'registration', JPATH_BASE. '/components/com_users/models' );
+		$model = JModelLegacy::getInstance('Registration', 'UsersModel');
+
+		$return = $model->activate($token);
+
+
+		// Check for errors. If couldn't activate, then return error message
+		if ($return === false) {
+			// Redirect back to the homepage.
+			//$app->redirect("",JText::sprintf('COM_USERS_REGISTRATION_SAVE_FAILED', $model->getError()),'warning');
+			return true;
+		}
+
+		// If activation is enabled and we are here if we could
+		// activate the account, then perform the login
+		// after login restore the password and redirect
+
+		if ($uParams->get('useractivation') == 1) {
+			$credentials = array ('username'=>$result->username, 'password'=>$temp_pass);
+			$result = $app->login($credentials);
+			//if (!($result instanceof Exception)) {
+			$redirect_url = $app->getUserState('users.login.form.return', 'index.php');
+
+			$app->redirect($redirect_url);
+			//}
+		}
+
+		return true;
+
+	}
+
+
+	/**
+	 * If needed restore original password and redirect
+	 *
+	 * Full description (multiline)
+	 *
+	 * @author Gruz <arygroup@gmail.com>
+	 * @param	type	$name	Description
+	 * @return	type			Description
+	 */
+	public function onUserLogin($userarr, $options)	{
+
+		$app	= JFactory::getApplication();
+		// We do not need to do anything, if the user is logged in
+		if (!JFactory::getUser()->guest) {return true;}
+		if ($app->isAdmin()) {return true;}
+
+		$db		= JFactory::getDbo();
+		$query	= $db->getQuery(true);
+
+		// Get regular joomla user object via getting user id via DB query
+		$query->select('id');
+		$query->from('#__users');
+		$query->where('username=' . $db->Quote($userarr['username']));
+
+		$db->setQuery($query);
+		$result = $db->loadObject();
+		$user = JFactory::getUser($result->id);
+
+		if ($this->params->get('autologinonactivate', 1)) {
+			// Restore original password previously stored in session
+			$session = JFactory::getSession();
+			// Get previously stored user password
+			$origPass = $session->get('origPass', false, $this->session_namespace);
+			if ($origPass === false ){
+				//return true;
+			} else {
+				// Set original user password into the HUser object
+				$session->clear('origPass', $this->session_namespace);
+				$user->password = $origPass;
+			}
+		}
+
+		// Get redirect link from either POST/GET/QUERY (lower priority)
+		// or from the user params (higher priority). Flush the user parameter
+		// So we use either the passed in some way redirect link, or
+		// use the stored in the user DB record returnLink
+		$jinput = JFactory::getApplication()->input;
+		$redirect_url = $user->getParam('returnLink', null);
+		$user->setParam('returnLink', null);
+		if (!$redirect_url) {
+			$redirect_url = $session->get('previous_link', false, $this->session_namespace);
+			if (!$redirect_url) {
+				$redirect_url = $jinput->get('return',false);
+				$redirect_url = base64_decode($redirect_url);
+			}
+		}
+		if (!empty ($redirect_url)) {
+			// Set the redirect link to the users.login.form.return which will ab automatically used by Joomla
+			// ALAS redirect here doesn't work in this way.
+			$app->setUserState('users.login.form.return', $redirect_url);// Doesn't work
+			// So we must redirect in a hardcore way:
+			//$app->redirect(JRoute::_($redirect_url, false));
+			//$app->redirect(JRoute::_($app->getUserState('users.login.form.return'), false));
+		}
+		$user->save();
+
+		return true;
+	}
+}
+
+?>
